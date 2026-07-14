ed73ed4bd35a87d2e5deddd92c75dd8a159ac438
LOGESH-E-07 <166407430+LOGESH-E-07@users.noreply.github.com>
2026-07-14 22:03:20 +0530

    go.js

--- api/go.js
+++ api/go.js
@@ -1,1 +1,18 @@
+export default function handler(req, res) {
+  const urls = [
+    "https://www.amazon.in/s?k=laptop+bags+for+man&rh=p_78%3AB0GF29D8Q6&ref=nb_sb_noss",
+    "https://www.amazon.in/s?k=laptop+messenger+bag+for+men&rh=p_78%3AB0GF29D8Q6&ref=nb_sb_noss",
+    "https://www.amazon.in/s?k=office+bag+for+men&rh=p_78%3AB0GF29D8Q6&ref=nb_sb_noss"
+  ];
 
+  const randomUrl = urls[Math.floor(Math.random() * urls.length)];
+
+  res.writeHead(302, {
+    Location: randomUrl,
+    "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+    Pragma: "no-cache",
+    Expires: "0"
+  });
+
+  res.end();
+}
